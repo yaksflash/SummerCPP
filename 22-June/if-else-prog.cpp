099ba88f9e5dfc692bf5f45f6a2f0b1e98c74bb4
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-22 11:29:08 +0300

    Первый коммит 22.06.2025

---
+++ 22-June/if-else-prog.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+// using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU"); // Установить русский язык для консоли
+
+    std::cout << "Введите ваш возраст: ";
+    
+    int age;
+    std::cin >> age;
+
+    if (age < 18)
+        std::cout << "Вы весьма молоды :)";
+    else if (age == 18)
+        std::cout << "Сколько-сколько? 18?)";
+    else if (age < 60)
+        std::cout << "Вам меньше 60!";
+    else
+        std::cout << "Ого!";
+    
+
+    return 0; // Знак об успешном выполнении программы
+}
