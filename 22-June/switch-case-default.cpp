254b42d1db64a43aadc1436923dff7847ba77b27
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-22 15:00:42 +0300

    Switch/Case - второй коммит 22.06.2025

---
+++ 22-June/switch-case-default.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+// using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU"); // Установить русский язык для консоли
+
+    std::cout << "Введите число: ";
+    int num;
+    std::cin >> num;
+
+    switch (num) {
+    case 1:
+        std::cout << "1";
+        break;
+    case 2:
+        std::cout << "2";
+        break;
+    default:
+        std::cout << "else";
+        break;
+    }
+
+    return 0; // Знак об успешном выполнении программы
+}
