4f821e6210b4aef0e1d3580a0b6c1033457cd5ce
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-24 12:34:47 +0300

    Первый коммит 24.06.2024 | Практика цикла while

---
+++ 24-June/cycle-while.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Практика цикла while. Программа перебирает значения num и выводит в консоль.
+    В конце добавляется фраза "может быть следующее число ...".
+    Если следующее число есть - сначала выводится подтверждение :)  
+    */
+
+    // НЕ УЧТЕНО: Цикл может стать бесконечным :}
+
+    setlocale(LC_ALL, "RU");
+
+    float num, last, change, first;
+
+    cout << "Введите начальное (меньшее) значение: ";
+    cin >> num;
+
+    cout << "Введите конечное (большее) значение: ";
+    cin >> last;
+
+    cout << "Введите изменение (положительное число): ";
+    cin >> change;
+
+    if (num > last || change == 0) {
+        cout << "ЯВНО НЕВЕРНЫЙ ВВОД!";
+        return -1;
+    }
+
+    first = num;
+
+    while (num <= last) {
+        if (first != num)
+            cout << "YES!" << endl;
+
+        cout << "NUM: " << num << endl << endl;
+        cout << "Maybe next " << num + change << "?" << endl;
+
+        num += change;
+    }
+
+    cout << "No more!";
+
+}
