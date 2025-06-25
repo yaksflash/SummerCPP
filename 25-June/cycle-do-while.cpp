45196c9fb246bfb37ccd7c4c4227d56ae9608270
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-25 22:18:32 +0300

    25.06.2025 | Do-while цикл

---
+++ 25-June/cycle-do-while.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int num, change, end;
+
+    cout << "Введите число: ";
+    cin >> num;
+
+    cout << "Введите изменение: ";
+    cin >> change;
+
+    cout << "Введите конечное значение: ";
+    cin >> end;
+
+    if (change < 0) {
+        change *= -1;
+        cout << "Change теперь положительное!";
+    }
+
+    cout << "Element: " << num << endl;
+
+    do {
+        num -= change;
+        cout << "Element: " << num << endl;
+    } while (num - change > end);
+
+}
