621a5e749fd13ec57b5fbd2aadc8609bd7ca812e
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-27 21:20:05 +0300

    27.06.2025 | Первый коммит: практика массивов #1

---
+++ 27-June/little-array-practice.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter element #" << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    for (int i = 0; i < 5; i++) 
+        cout << "Element #" << i + 1 << " - " << numbers[i] << endl;
+    
+}
