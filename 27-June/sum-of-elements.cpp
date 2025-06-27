692bd7317672eb8543f0bfe05856cd70fb78568f
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-27 21:27:01 +0300

    Второй коммит - сумма элементов массива

---
+++ 27-June/sum-of-elements.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numbers[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter element #" << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    int summa = 0;
+    for (int j = 0; j < 5; j++)
+        summa += numbers[j];
+
+    cout << "Sum: " << summa;
+    
+
+}
