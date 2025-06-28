3474e6ea5742350bed91bbbf801ab8dbb7bba08f
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-28 21:42:53 +0300

    Работа с динамическим массивом

---
+++ 28-June/newInt-dynamicArray.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+int main()
+{
+    int count;
+
+    std::cout << "Enter count of elements: ";
+    std::cin >> count;
+
+    int* nums = new int[count];
+
+    int user_input;
+
+    for (int i = 0; i < count; i++) {
+        std::cout << "Enter value: ";
+        std::cin >> user_input;
+        nums[i] = user_input;
+    }
+
+    std::cout << std::endl;
+
+    for (int i = 0; i < count; i++)
+        std::cout << "El: " << nums[i];
+
+    delete[] nums;
+
+    /*
+    При попытке запуска кода:
+
+    for (int i = 0; i < count; i++)
+        std::cout << "El: " << nums[i];
+        
+    Возникает ошибка, так как помять с nums была ранее очищена.
+    Всё работает корректно!
+    */
+    
+}
