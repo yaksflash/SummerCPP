ff91731d10762f9a8c42f95d181a6eaffdb99faa
Maxim Gubarev <yaksflash@yandex.ru>
2025-06-30 21:06:05 +0300

    Работа с динамическими массивами - новый аналог

---
+++ 30-June/correct-dynamic-arrays.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> nums;
+    int user_input;
+
+    do {
+        cout << "Enter num (\"0\" to end): ";
+        cin >> user_input;
+
+        if (user_input != 0)
+            nums.push_back(user_input);
+    } while (user_input != 0);
+
+    cout << "READY!";
+
+    for (int i = 0; i < nums.size(); i++) 
+        cout << "El #" << i << " - " << nums[i] << endl;
+
+    return 0;
+}
