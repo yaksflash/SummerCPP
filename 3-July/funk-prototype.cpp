2563fd6dd80b4ca178eeb915339c60c3a3f3c5c6
Maxim Gubarev <yaksflash@yandex.ru>
2025-07-03 21:19:56 +0300

    Прототип функции

---
+++ 3-July/funk-prototype.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int factorial(int number); // прототип функции
+
+int main()
+{
+    std::cout << "Enter num: ";
+    int num;
+    std::cin >> num;
+
+    std::cout << "Factorial is " << factorial(num);
+}
+
+int factorial(int number) {
+    int res = 1;
+
+    for (int i = 1; i <= number; i++)
+        res *= i;
+
+    return res;
+}
