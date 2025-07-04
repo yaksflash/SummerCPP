c6c78fcc78c4d6bebdc0d057527d8d3a48d90902
Maxim Gubarev <yaksflash@yandex.ru>
2025-07-04 18:36:09 +0300

    Ссылки и указатели

---
+++ 4-July/pointers_and_reference.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int minValueOfArray(int * array, int len) {
+    int min = *array;
+    for (int i = 0; i < len; i++) {
+        if (min > *(array + i)) 
+            min = *(array + i);
+    }
+
+    return min;
+}
+
+int main()
+{
+    int count, user_input;
+    vector<int> numbers;
+    
+    cout << "Enter len of array: ";
+    cin >> count;
+    
+    while (count != 0) {
+        cout << "Enter val: ";
+        cin >> user_input;
+
+        numbers.push_back(user_input);
+
+        count--;
+    }
+
+    
+    cout << "Min value: " << minValueOfArray(numbers.data(), numbers.size());
+
+    return 0;
+}
