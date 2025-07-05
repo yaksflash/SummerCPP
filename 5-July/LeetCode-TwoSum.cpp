5c9f3e2a2a62362296401b0fc4a1b3521d6db65a
Maxim Gubarev <yaksflash@yandex.ru>
2025-07-05 10:57:25 +0300

    LeetCode #1

---
+++ 5-July/LeetCode-TwoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i ++) {
+            for (int j = 0; j < nums.size(); j ++) {
+                if (i == j) 
+                    continue;
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+
+        return {};
+    }
+};
